51acccae83f321aac4a1c802a780431973ece87d
Adrian Balko <adrianbalko7777@gmail.com>
2026-03-10 08:15:07 +0100

    rgb svtetlo

---
+++ PythonProject5/pixels farby green.py
@@ -0,0 +1,11 @@
+from PIL import Image
+
+
+pic = Image.open('les.jpg')
+pixels = pic.load()
+for y in range(pic.size[1]):
+   for x in range(pic.size[0]):
+       temp = pixels[x,y] #(343,151,155)
+       pixels[x,y] = (0,temp[1],0)
+
+pic.show()
